c5efb8d33adadb74144cbdff9c908522310d4eb7
Ben663 <87435130+Ben663@users.noreply.github.com>
2023-06-04 21:02:21 +0300

    .

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+import { Task } from './task';
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.scss']
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'myapp';
+  tasks: Task[] = [
+    { title: 'Task 1', completed: false },
+    { title: 'Task 2', completed: true },
+    { title: 'Task 3', completed: false }
+  ];
+
+  addTask(title: string) {
+    this.tasks.push({ title, completed: false });
+  }
 }
